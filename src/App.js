2073583e01da979ac99c93326eb34fd072229c90
Pankaj kumar <pankaj.vayuz@gmail.com>
2023-08-30 15:36:48 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,46 @@ function App() {
   return (
     <div className="bg-gray-100">
       <header className="bg-blue-500 p-4 text-white">
-        <h1 className="text-3xl font-semibold">Welcome to our Tour & Travel Website</h1>
+        <h1 className="text-3xl font-semibold">Welcome to Happu Tour & Travel</h1>
       </header>
       
       <main className="container mx-auto p-4">
-        <h2 className="text-2xl font-semibold mb-4">Featured Tours</h2>
-        {/* Add your tour cards or listings here */}
+       
+        {/* Add your tour cards or listings here */}<div className="bg-gray-100">
+  {/* ... Header and other sections ... */}
+  
+  <main className="container mx-auto p-4">
+    <h2 className="text-2xl font-semibold mb-4">Featured Tours</h2>
+    <div className="grid grid-cols-3 gap-4">
+      {/* Tour Card 1 */}
+      <div className="bg-white rounded shadow-md p-4">
+        <img src="tour1.jpg" alt="Tour 1" className="w-full h-auto mb-2" />
+        <h3 className="text-xl font-semibold mb-1">Adventure Excursion</h3>
+        <p className="text-gray-600">Explore thrilling mountains and forests.</p>
+        <p className="text-blue-500 font-semibold">$499</p>
+      </div>
+      
+      {/* Tour Card 2 */}
+      <div className="bg-white rounded shadow-md p-4">
+        <img src="tour2.jpg" alt="Tour 2" className="w-full h-auto mb-2" />
+        <h3 className="text-xl font-semibold mb-1">Beach Getaway</h3>
+        <p className="text-gray-600">Relax on beautiful sandy beaches.</p>
+        <p className="text-blue-500 font-semibold">$299</p>
+      </div>
+      
+      {/* Tour Card 3 */}
+      <div className="bg-white rounded shadow-md p-4">
+        <img src="tour3.jpg" alt="Tour 3" className="w-full h-auto mb-2" />
+        <h3 className="text-xl font-semibold mb-1">Cultural Exploration</h3>
+        <p className="text-gray-600">Immerse in rich local traditions.</p>
+        <p className="text-blue-500 font-semibold">$399</p>
+      </div>
+    </div>
+  </main>
+  
+  {/* ... Contact and footer sections ... */}
+</div>
+
         
         <h2 className="text-2xl font-semibold mt-8 mb-4">Vehicle Image Gallery</h2>
         <div className="grid grid-cols-3 gap-4">
@@ -26,14 +60,19 @@ function App() {
         <div className="grid grid-cols-2 gap-4">
           <div>
             <h3 className="text-xl font-semibold mb-2">Location</h3>
-            <p>Your Company Name</p>
-            <p>123 Main Street</p>
-            <p>City, Country</p>
+            <p>Happu Tour and Travel</p>
+            <p>Noida</p>
+            <p>Delhi, India</p>
           </div>
           <div>
             <h3 className="text-xl font-semibold mb-2">Email</h3>
             <p>contact@example.com</p>
+            <div>
+            <h3 className="text-xl font-semibold mb-2">Phone</h3>
+            <p>+919956789656</p>
           </div>
+          </div>
+          
         </div>
       </main>
       
